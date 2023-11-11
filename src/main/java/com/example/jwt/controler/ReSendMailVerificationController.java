8d5f18ca4250c2572991824dea3e2eaff4f02989
Raj Kumar <rajkumariimt2002@gmail.com>
2023-11-11 15:00:27 +0530

    resend

--- src/main/java/com/example/jwt/controler/ReSendMailVerificationController.java
+++ src/main/java/com/example/jwt/controler/ReSendMailVerificationController.java
@@ -5,6 +5,7 @@ import com.example.jwt.registration.token.VerificationTokenRepository;
 import com.example.jwt.service.UserService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -67,8 +68,44 @@ public class ReSendMailVerificationController {
 //    }
 
 
+    //FINAL THA
+
+//    @GetMapping("/resendVerification")
+//    public String resendVerificationEmail(@RequestParam("email") String email) {
+//        // Step 1: Search for a user with the provided email
+//        Optional<User> userOptional = userService.findByEmail(email);
+//
+//        if (userOptional.isPresent()) {
+//            User theUser = userOptional.get();
+//
+//            // Check if the user's email is already verified
+//            if (theUser.isEmailVerified()) {
+//                return "Email has already been verified. Please log in.";
+//            }
+//
+//            // Step 2: Generate a new verification token
+//            String newVerificationToken;
+//            do {
+//                newVerificationToken = generateNewVerificationToken();
+//            } while (tokenRepository.findByToken(newVerificationToken) != null);
+//
+//            // Step 3: Update the user's token in the database
+//            userService.saveUserVerificationToken(theUser, newVerificationToken);
+//
+//            // Step 4: Send the new verification email with the new token using your EmailService
+//            sendVerificationEmail(theUser, newVerificationToken);
+//
+//            return "A new verification email has been sent. Please check your email.";
+//        } else {
+//            return "User not found. Please check the email address.";
+//        }
+//    }
+//
+
+
+
     @GetMapping("/resendVerification")
-    public String resendVerificationEmail(@RequestParam("email") String email) {
+    public String resendVerificationEmail(@RequestParam("email") String email, HttpServletRequest request) {
         // Step 1: Search for a user with the provided email
         Optional<User> userOptional = userService.findByEmail(email);
 
@@ -89,15 +126,17 @@ public class ReSendMailVerificationController {
             // Step 3: Update the user's token in the database
             userService.saveUserVerificationToken(theUser, newVerificationToken);
 
-            // Step 4: Send the new verification email with the new token using your EmailService
-            sendVerificationEmail(theUser, newVerificationToken);
+            // Step 4: Get the base URL of the application
+            String appUrl = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
+
+            // Step 5: Send the new verification email with the new token using your EmailService
+            sendVerificationEmail(theUser, newVerificationToken, appUrl);
 
             return "A new verification email has been sent. Please check your email.";
         } else {
             return "User not found. Please check the email address.";
         }
     }
-
 
 
 //    @GetMapping("/resend/verifyEmail")
@@ -146,16 +185,39 @@ public class ReSendMailVerificationController {
     }
 
 
+// FINAL THA
+
+//    public void sendVerificationEmail(User theUser, String verificationToken) {
+//        String subject = "Email Verification";
+//        String senderName = "Nutrify India Now (2.O)";
+//        String mailContent = "<p> Hi, " + theUser.getEmail() + ", </p>" +
+//                "<p>Thank you for registering with us. Please follow the link below to complete your registration:</p>" +
+//                "<a href=\"http://localhost:7073/register/verifyEmail?token=" + verificationToken + "\">Verify your email to activate your account</a>" +
+//                "<p> Thank you <br> Users Registration Portal Service";
+//
+//
+//        try {
+//            MimeMessage message = mailSender.createMimeMessage();
+//            var messageHelper = new MimeMessageHelper(message);
+//            messageHelper.setFrom("rajkumariimt2002@gmail.com", senderName);
+//            messageHelper.setTo(theUser.getEmail());
+//            messageHelper.setSubject(subject);
+//            messageHelper.setText(mailContent, true);
+//            mailSender.send(message);
+//        } catch (MessagingException | UnsupportedEncodingException e) {
+//            // Handle the exceptions here, for example, log the error or throw a custom exception
+//            throw new RuntimeException("Error sending email", e);
+//        }
+//    }
 
 
-    public void sendVerificationEmail(User theUser, String verificationToken) {
+    public void sendVerificationEmail(User theUser, String verificationToken, String applicationUrl) {
         String subject = "Email Verification";
         String senderName = "Nutrify India Now (2.O)";
         String mailContent = "<p> Hi, " + theUser.getEmail() + ", </p>" +
                 "<p>Thank you for registering with us. Please follow the link below to complete your registration:</p>" +
-                "<a href=\"http://localhost:7073/register/verifyEmail?token=" + verificationToken + "\">Verify your email to activate your account</a>" +
+                "<a href=\"" + applicationUrl + "/register/verifyEmail?token=" + verificationToken + "\">Verify your email to activate your account</a>" +
                 "<p> Thank you <br> Users Registration Portal Service";
-
 
         try {
             MimeMessage message = mailSender.createMimeMessage();
@@ -170,6 +232,7 @@ public class ReSendMailVerificationController {
             throw new RuntimeException("Error sending email", e);
         }
     }
+
 
 
 
