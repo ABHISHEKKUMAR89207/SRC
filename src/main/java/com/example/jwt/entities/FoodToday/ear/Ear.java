9abbd5efd525e1f4ce142cd0ffa3301a3215b884
unknown <anujsrivastvaak47@gmail.com>
2024-02-22 11:28:56 +0530

    dasbdffhj

--- src/main/java/com/example/jwt/entities/FoodToday/ear/Ear.java
+++ src/main/java/com/example/jwt/entities/FoodToday/ear/Ear.java
@@ -28,7 +28,7 @@ public class Ear {
 
     private double energy;
     private double protein;
-    private boolean fatsOilsVisible;
+    private boolean fats;
     private double fiber;
     private double calcium;
     private double magnesium;
